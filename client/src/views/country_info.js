62ac5d74382293f246105236ab53bf4388c18b42
Rob <robdwilliams85@gmail.com>
2017-12-21 10:32:07 +0000

    render country and removed bundle.js

--- client/src/views/country_info.js
+++ client/src/views/country_info.js
@@ -4,6 +4,11 @@ const CountryInfo = function () {
 
 CountryInfo.prototype.render = function (country) {
   // TODO: Render country info
+  const ul = document.querySelector('#country-info')
+  const li = document.createElement('li')
+  const select = document.querySelector('#countries-select')
+  li.innerText = select.option
+  ul.appendChild(li)
 }
 
 CountryInfo.prototype.createListItem = function (label, content) {
